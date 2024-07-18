fd47ad8affed4284597abc4bd07d34bbed55a7d2
Wolfes <166002502+DwiDevelopes@users.noreply.github.com>
2024-07-18 13:29:11 +0700

    Multimedia 5
    Anak berprestasi dan bertalenta

---
+++ js/custom.js
@@ -0,0 +1,57 @@
+/*!
+ * Bootstrap  v5.2.2
+ * Copyright 2024 The Bootstrap Authors Dwi Bakti N Dev
+ * Copyright By Dwi Bakti N Dev
+ * Licensed Dwi Bakti N Dev
+ * Website ini di buat oleh dwi bakti n dev dan sebagai kenangan semasa sekolah di Smkn 6 Kab Tanggerang Dan sebagai ucapan terima kasih saya pada sekolah tersebut dalam membimbing muridnya*/
+  (function ($) {
+  
+  "use strict";
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height();
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-0;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+
+    $('.owl-carousel').owlCarousel({
+        center: true,
+        loop: true,
+        margin: 30,
+        autoplay: true,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items: 2,
+            },
+            767:{
+                items: 3,
+            },
+            1200:{
+                items: 4,
+            }
+        }
+    });
+  
+  })(window.jQuery);
+
+
